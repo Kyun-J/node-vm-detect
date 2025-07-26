d6843f9de8b03ce2c845e47adebce81614bb3b6b
Kyun-J <dvkyun@gmail.com>
2025-07-26 16:39:37 +0900

    test

--- scripts/build-win-arm64.js
+++ scripts/build-win-arm64.js
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 
 fs.mkdirSync('./dist/win/arm64', { recursive: true });
-execSync('node-gyp rebuild --arch=arm64');
+execSync('node-gyp rebuild --arch=arm64 --verbose');
 fs.copyFileSync(
   './build/Release/detectWindows.node',
   './dist/win/arm64/detectWindows.node',
