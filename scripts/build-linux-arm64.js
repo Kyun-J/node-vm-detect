3a5f178bd8ba192d1ded8888d28c74ff780d1c31
Kyun-J <dvkyun@gmail.com>
2025-07-26 20:01:50 +0900

    test

--- scripts/build-linux-arm64.js
+++ scripts/build-linux-arm64.js
@@ -1,13 +1,6 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-fs.mkdirSync('./dist/linux/x64', { recursive: true });
-execSync('node-gyp rebuild --arch=x64');
-fs.copyFileSync(
-  './build/Release/detectLinux.node',
-  './dist/linux/x64/detectLinux.node',
-);
-
 fs.mkdirSync('./dist/linux/arm64', { recursive: true });
 execSync('node-gyp rebuild --arch=arm64');
 fs.copyFileSync(
--- scripts/build-test.js
+++ scripts/build-test.js
@@ -5,13 +5,15 @@ execSync('npm run bundle');
 if (process.platform === 'win32') {
   if (process.arch === 'x64') {
     execSync('npm run gyp:win:x64');
-  } else if (process.arch === 'arm64') {
-    execSync('npm run gyp:win:arm64');
   }
 } else if (process.platform === 'darwin') {
   execSync('npm run gyp:mac');
 } else if (process.platform === 'linux') {
-  execSync('npm run gyp:linux');
+  if (process.arch === 'x64') {
+    execSync('npm run gyp:linux:x64');
+  } else if (process.arch === 'arm64') {
+    execSync('npm run gyp:linux:arm64');
+  }
 }
 
 const test = async () => {
