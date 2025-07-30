628f74c3c6254b14605ae9421e339b2f40f75f42
Kyun-J <dvkyun@gmail.com>
2025-07-31 01:30:28 +0900

    windows arm test

--- scripts/build-test.js
+++ scripts/build-test.js
@@ -5,6 +5,8 @@ execSync('npm run bundle');
 if (process.platform === 'win32') {
   if (process.arch === 'x64') {
     execSync('npm run gyp:win:x64');
+  } else if (process.arch === 'arm64') {
+    execSync('npm run gyp:win:arm64');
   }
 } else if (process.platform === 'darwin') {
   execSync('npm run gyp:mac');
